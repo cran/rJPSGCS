cd02315593d2ca2efa50ba2ff8b49823e2efff28
Brad McNeney <mcneney@sfu.ca>
2018-05-13 15:12:41 +0000

    version 0.2-10

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,12 @@
 .onLoad <- function(libname, pkgname) {
   .jpackage(pkgname, lib.loc = libname)
   jv <- .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
-  if(substr(jv, 1L, 1L) == "1") {
-     jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
-     if(jvn < 1.8) stop("Java 8 is needed for this package but not available")
+  if(substr(jv, 1L, 2L) == "1.") {
+    jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
+    if(jvn < 1.8) stop("Java >= 8 is needed for this package but not available")
   }
   library.dynam("rJPSGCS", pkgname,libname)
-#  methods:::bind_activation(TRUE)
 }
 
 .Last.lib <- function(libpath) {
-#  methods:::bind_activation(FALSE)
 }
